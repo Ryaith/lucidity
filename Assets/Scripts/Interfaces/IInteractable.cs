b53e081798f7e9c0265a30ac08af43e8dfe5a377
Rahman Ahmed <rahmed83@uwo.ca>
2020-12-17 10:32:42 -0500

    Basic Interaction ands Plugins
    Added Basic Interaction System. Also added latest version of Github for Unity plugin and imported TMPro assets. Reordered folder and file structure.

---
+++ Assets/Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+
+    //Name of the Object
+    string getDisplayName();
+
+    //Name of the action to be performed (currently not used)
+    string getActionName();
+
+     void Activate();
+
+}
---
+++ Assets/Scripts/Objects/Switch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour, IInteractable
+{
+    public string displayName;
+
+    public string actionName;
+    public string getDisplayName()
+    {
+        return displayName;
+    }
+
+    public string getActionName()
+    {
+        return actionName;
+    }
+
+    [SerializeField] private GameObject activationObject;
+
+
+    public void Activate()
+    {
+        bool isActive = activationObject.activeSelf;
+        if (isActive)
+        {
+            activationObject.SetActive(false);
+        }
+        else
+        {
+            activationObject.SetActive(true);
+        }
+    }
+}
