bbf4733e81cdc6c9a0f9f6daad49036388df9875
Hayman Abdeljabbar <habdelj@uwo.ca>
2020-12-22 15:01:54 -0500

    Inner Demons Scene

---
+++ Assets/Scripts/InnerDemon/DemonInteract.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonInteract : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string displayName = "";
+    [SerializeField] private string actionName;
+
+    public void Activate()
+    {
+        Destroy(gameObject);
+    }
+
+    public string getActionName()
+    {
+        return actionName;
+    }
+
+    public string getDisplayName()
+    {
+        return displayName;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Scripts/InnerDemon/InnerDemonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerDemonManager : MonoBehaviour
+{
+    public string id;
+    private Vector3 axis = new Vector3(0,1,1); // axis which to rotate
+    private float angle = 2; // speed of rotation 
+    private int innerDemonCount = 0;
+
+    private void Update() 
+    {
+        transform.RotateAround(gameObject.transform.position, axis, angle);
+    }
+
+
+}
+
+
+
+
