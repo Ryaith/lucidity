0e0b175ee6d860e6287d62dd6934a9b6c1a5db17
Ryaith <19347142+Ryaith@users.noreply.github.com>
2020-12-22 15:46:17 -0500

    Boss testing scenes
    Added in the new boss and scene to demo it

---
+++ Assets/Scripts/Objects/BossZoneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossZoneScript : MonoBehaviour
+{
+
+    public GameObject boss;
+    private GeckoController_Full boss_controller;
+
+    private void Start()
+    {
+        boss_controller = boss.GetComponentInChildren<GeckoController_Full>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            boss_controller.SetRootMotion(true);
+        }
+    }
+
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            boss_controller.SetRootMotion(false);
+        }
+    }
+}
