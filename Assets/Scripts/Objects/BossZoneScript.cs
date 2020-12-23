078f4016c2b6e14b9b307b7545bc0d849b5c762b
Ryaith <19347142+Ryaith@users.noreply.github.com>
2020-12-22 20:33:12 -0500

    Update BossZoneScript.cs

--- Assets/Scripts/Objects/BossZoneScript.cs
+++ Assets/Scripts/Objects/BossZoneScript.cs
@@ -8,11 +8,13 @@ public class BossZoneScript : MonoBehaviour
     public GameObject boss;
     private GeckoController_Full boss_controller;
 
+    //Grab the boss_controller script from the boss at start
     private void Start()
     {
         boss_controller = boss.GetComponentInChildren<GeckoController_Full>();
     }
 
+    //Set the boss controller to movement when player enter the boss zone
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -21,7 +23,7 @@ public class BossZoneScript : MonoBehaviour
         }
     }
 
-    //When the Primitive exits the collision, it will change Color
+    //Set the boss controller to no movement when player leaves the boss zone
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
