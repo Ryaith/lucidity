af11d047a797f14ddab6e6fd63005213b5a820e7
Rahman Ahmed <rahmed83@uwo.ca>
2021-01-30 16:23:55 -0500

    Teleporter created, started with message box

---
+++ Assets/Scripts/Objects/Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public GameObject destinationPoint;
+    void OnTriggerEnter(Collider col)
+    {
+        if (destinationPoint)
+        {
+            GameObject target = col.gameObject;
+            Teleport(target);
+        }
+    }
+    
+
+
+    private void Teleport(GameObject target)
+    {
+        target.SetActive(false);
+        target.transform.position = destinationPoint.transform.position;
+        target.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI MessageBox;
+
+    [SerializeField]
+    private float messageDisplayTime = 5f;
+
+    public void DisplayMessage(string message)
+    {
+        StartCoroutine(DisplayMessageForTime(message, messageDisplayTime));
+    }
+
+    public void DisplayMessage(string message, float time)
+    {
+        StartCoroutine(DisplayMessageForTime(message, time));
+    }
+
+
+
+
+    IEnumerator DisplayMessageForTime(string message, float time)
+    {
+        MessageBox.text = message;
+        MessageBox.enabled = true;
+        yield return new WaitForSeconds(time);
+        MessageBox.enabled = false;
+        
+    }
+
+
+}
