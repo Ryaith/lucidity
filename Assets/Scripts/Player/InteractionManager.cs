4e19a17d7ced007b2d9cb6980b18f149d7768e23
Rahman Ahmed <rahmed83@uwo.ca>
2021-02-01 19:33:43 -0500

    Pit Puzzle Created, Folders Organized, New MessageBox interaction

--- Assets/Scripts/Player/InteractionManager.cs
+++ Assets/Scripts/Player/InteractionManager.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractionManager : MonoBehaviour
 {
     public GameObject holdDest;
 
+    [SerializeField]
+    private TextMeshProUGUI messageBox;
+
     public GameObject getHoldingDestObject()
     {
         return holdDest;
+    }
+
+
+    public void PrintMessageForSeconds(string message, float time)
+    {
+        messageBox.enabled = true;
+        messageBox.text = message;
+        StartCoroutine(messageboxForSeconds(time));
+    }
+
+    IEnumerator messageboxForSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+        messageBox.enabled = false;
     }
 
 }
