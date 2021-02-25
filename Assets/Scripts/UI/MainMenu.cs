02ac8e33df0b3293a3ba3ef33356b98b488c2883
helloworld <cfinnie3@uwo.ca>
2021-02-25 09:40:38 -0500

    Main Menu
    Added a main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu: MonoBehaviour{
+				public int destination;
+				public GameObject helpInfo;
+				int helpActive = 0;
+				public void Exit() {
+								Application.Quit();
+				}
+				public void Play() {
+								SceneManager.LoadScene(destination);
+				}
+				public void Help() {
+								if (helpActive==0) {
+												helpInfo.SetActive(true);
+												helpActive = 1;
+								}
+								else {
+												helpInfo.SetActive(false);
+												helpActive = 0;
+								}
+								
+				}
+}
