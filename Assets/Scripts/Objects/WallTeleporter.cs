73e9215812264cc25bbda3ff2a34023347545ce9
helloworld <cfinnie3@uwo.ca>
2021-03-10 19:01:20 -0500

    Lost Forest Puzzle
    Made a puzzle that you walk through walls to get to the end

---
+++ Assets/Scripts/Objects/WallTeleporter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTeleporter : MonoBehaviour{
+				[SerializeField] private GameObject destination;
+
+				private void OnTriggerEnter(Collider other) {
+								if (other.tag == "Player") {
+												other.transform.position = destination.transform.position;
+												Physics.SyncTransforms();
+								}
+				}
+}
