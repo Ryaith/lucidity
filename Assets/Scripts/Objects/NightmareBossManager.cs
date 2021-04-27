e786a72474484c9d5a8bccf6c4453724ad506b79
Quinn Fisher <quinnfisher@hotmail.com>
2021-04-26 23:46:24 -0400

    Initial nightmare level push with probably 4 viable pushes of content

---
+++ Assets/Scripts/Objects/NightmareBossManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightmareBossManager : MonoBehaviour
+{
+    GameObject innerDemon;
+    Turret boss;
+
+    [SerializeField] private int numSwitches;
+    bool[] hits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss = GetComponentInChildren<Turret>();
+        innerDemon = GetComponentInChildren<InnerDemonManager>().gameObject;
+        innerDemon.SetActive(false);
+        hits = new bool[numSwitches];
+    }
+
+    public void hit(int switchid)
+    {
+        hits[switchid] = true;
+        bool dead = true;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == false)
+            {
+                dead = false;
+            }
+        }
+        if (dead)
+        {
+            boss.enabled = false;
+            innerDemon.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Sample/NightmareBossSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightmareBossSwitch : Switch
+{
+    public int switchid;
+    NightmareBossManager boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss = GetComponentInParent<NightmareBossManager>();
+    }
+
+    public override void Activate(GameObject player)
+    {
+        boss.hit(switchid);
+    }
+}
