4a5597574ed28eefa7095e08d362a54a51333514
Hayman AbdelJabbar <hayman-35@hotmail.com>
2021-04-27 22:17:33 -0400

    Reworked area

--- Assets/Hayman_Level/GravityAttractor.cs
+++ Assets/Hayman_Level/GravityAttractor.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class GravityAttractor : MonoBehaviour {
 	
 	public float gravity = -9.8f;
-	
-	
+
+
 	public void Attract(CharacterController body) {
 		Vector3 gravityUp = (body.transform.position - transform.position).normalized;
 		Vector3 localUp = body.transform.up;
 		
 		// Apply downwards gravity to body
 		//body.attachedRigidbody.AddForce(gravityUp * gravity);
+		
 		// Allign bodies up axis with the centre of planet
 		body.transform.rotation = Quaternion.FromToRotation(localUp,gravityUp) * body.transform.rotation;
-	}  
+	
+	}
 }
